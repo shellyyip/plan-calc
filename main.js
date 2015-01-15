79f3a686402e67eec910985e1683dc61ec927305
Shelly Njoo <shelly@shellynjoo.com>
2015-01-15 13:47:38 -0500

    Removing node_modules/

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 // p1b-digitalPlanConnections
 var USCP1B = USCP1B || {};
-console.log('******** starting p1b');
+var totalPlan = {
+	totalDeviceNum: 0,
+	totalCost: 0
+};
+var $plus = $('.spinner-plus');
+var $minus = $('.spinner-minus');
 
-// Devices
+// **** Devices
 var allDevices = [];
 $deviceList = $('ul.devices').find('li');
 $deviceList.each(function() {
 	allDevices.push($(this).data());
 });
 
-var totalPlan = {
-	totalDeviceNum: 0,
-	totalCost: 0
+// **** Data Plans
+USCP1B.DataPlan = function(gbs, cost) {
+	this.gbs = gbs;
+	this.cost = cost;
 };
+var gbs10 = new USCP1B.DataPlan(10, 90);
+var gbs12 = new USCP1B.DataPlan(12, 110);
+var gbs14 = new USCP1B.DataPlan(14, 120);
+var gbs16 = new USCP1B.DataPlan(16, 130);
+var gbs18 = new USCP1B.DataPlan(18, 140);
+var gbs20 = new USCP1B.DataPlan(20, 150);
 
-var $plus = $('.spinner-plus');
-var $minus = $('.spinner-minus');
-
-var preventNegNum = function() {
-	console.log('**************');
+// **** Functions 
+var toggleMinus = function() {// Checks if minus button will make quantity go negative, and toggles on/off accordingly
 	$deviceList.each(function() {
 		var $this = $(this);
 		var num = $this.data('num');
-		console.log('Quant of '+$this.data('device')+' is '+num);
 		if (num == 0) {
 			$this.find('.spinner-minus').prop('disabled',true);
 		} else {
@@ -44,26 +52,34 @@ var updateDeviceNum = function() {
 	if (total == 10) {
 		//Disable all plus buttons
 		$plus.prop('disabled', true);
-		preventNegNum();
+		toggleMinus();
 	} else if (total == 2) {
 		//Disable all minus buttons
 		$minus.prop('disabled', true);
 	} else {
 		$plus.prop('disabled', false);
-		preventNegNum();
+		toggleMinus();
 	}
 	// ** Update & print total
 	totalPlan.totalDeviceNum = total;
 	$('.total-devices').text(totalPlan.totalDeviceNum);
 }
+var calcCost = function() {
+	console.log(allDevices);
+}
+var updateAll = function() {
+	updateDeviceNum();
+	calcCost();
+}
 
+// **** Events 
 $plus.on('click', function() {
 	var device = $(this).parents('[data-device]').data('device');
 	var obj = $.grep(allDevices, function(e){ 
 		return e.device == device; 
 	});
 	obj[0]['num']+=1
-	updateDeviceNum();
+	updateAll();
 });
 $minus.on('click', function() {
 	var device = $(this).parents('[data-device]').data('device');
@@ -71,20 +87,8 @@ $minus.on('click', function() {
 		return e.device == device; 
 	});
 	obj[0]['num']-=1
-	updateDeviceNum();
+	updateAll();
 });
 
-
-// Data Plans
-USCP1B.DataPlan = function(gbs, cost) {
-	this.gbs = gbs;
-	this.cost = cost;
-};
-var gbs10 = new USCP1B.DataPlan(10, 90);
-var gbs12 = new USCP1B.DataPlan(12, 110);
-var gbs14 = new USCP1B.DataPlan(14, 120);
-var gbs16 = new USCP1B.DataPlan(16, 130);
-var gbs18 = new USCP1B.DataPlan(18, 140);
-var gbs20 = new USCP1B.DataPlan(20, 150);
-
-updateDeviceNum();
+// **** Init
+updateAll();
