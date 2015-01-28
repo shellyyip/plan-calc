febc7faa39002dea1483731a799113f74da386d4
Shelly Njoo <shelly@shellynjoo.com>
2015-01-28 17:11:45 -0500

    Find and flip different digits

--- digit-flipper.js
+++ digit-flipper.js
@@ -36,14 +36,15 @@ var flipThis = function(el) {
 	//Trigger Flip
 	el.flip(true);
 	//Destroy Flip when transition ends
-	// setTimeout(//need timeout instead of using ontranstionend because if user changes the number too fast, the end event can never fire
-	//     function(e) {
-	//     	el.unwrap();
-	//     	el.removeAttr('style');
-	//     	el.find('.front').remove();
-	//     	el.find('.back').children().unwrap();
-	//     }
-	// , 500);//set timeout equal to speed of flip
+	setTimeout(//need timeout instead of using ontranstionend because if user changes the number too fast, the end event can never fire
+	    function(e) {
+	    	el.unwrap();
+	    	el.removeAttr('style');
+	    	el.find('.front').remove();
+	    	el.text( el.find('.back').text() );
+	    	el.find('.back').empty();
+	    }
+	, 500);//set timeout equal to speed of flip
 }
 
 //Find digit element to flip. Pass in two elements with spans in them, find different spans, then call flip on them
@@ -55,6 +56,13 @@ var flipDiffDigit = function(parentElem) {
 		newDigits.each(function() {
 			flipThis( $(this) );
 		});
+	} else {
+	//Otherwise, compare each digit and flip the different one
+		for (var i=0;i<oldDigits.length;i++) {
+			if ( $(oldDigits[i]).text() != $(newDigits[i]).text() ) {
+				flipThis( $(newDigits[i]) );
+			}
+		}
 	}
 }
 
